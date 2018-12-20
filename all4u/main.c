0d0da0001ec09a0b8104d02ac0dfaa8d0ca390b6
lera <lera@localhost.localdomain>
2018-12-20 16:05:13 +0300

    всё для вас

--- all4u/main.c
+++ all4u/main.c
@@ -1,12 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include "btree.h"
 
 #define ERROR_CREATE_OR_OPEN_FILE -1
 #define ERROR_TYPE -2
 
 FILE * out;
+
+typedef struct TBtreeNode
+{
+    int value;
+    struct TBtreeNode *left;
+    struct TBtreeNode *right;
+} node;
+
+node *Add(node *nd, int value)
+{
+    if (nd == NULL)
+    {
+        node *new_nd = (node *)malloc(sizeof(node));
+        new_nd->left = NULL;
+        new_nd->right = NULL;
+        new_nd->value = value;
+        return new_nd;
+    }
+    else if (nd->value == value)
+        return nd;
+
+    if (nd->value < value)
+        nd->right = Add(nd->right, value);
+    else
+        nd->left = Add(nd->left, value);
+
+    return nd;
+}
+
+void printInorder(const node *tree, int tab)
+{
+    if (tree == NULL)
+    {
+        return;
+    }
+    printInorder(tree->left, tab + 2);
+    for (int i = 0; i < tab; ++i)
+    {
+        putchar(' ');
+    }
+    printf("%d\n", tree->value);
+    printInorder(tree->right, tab + 2);
+}
 
 void LeftRootRight(node *node)
 {
